bfc51b763083d6922b26711452ff452fcfe120f7
AJ Ibraheem <ibraheemaj@outlook.com>
2020-03-19 14:17:54 +0000

    Fix dataset length

--- data/voc_dataset.py
+++ data/voc_dataset.py
@@ -15,7 +15,7 @@ class VOCDataset(data.Dataset):
             self.image_paths = [x.strip() for x in file.readlines()]            
 
     def __len__(self):        
-        return 5#len(self.image_paths)
+        return len(self.image_paths)
 
     """
     Returns (Image, [det])
